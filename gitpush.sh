a4d9cdeb91e507ca4e1b231630aa3f7cb6d56b52
yomox9 <yomox9@gmail.com>
2015-10-19 14:27:52 +0900

    modified: gitpush.sh, comment_by_gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -1,10 +1,15 @@
-#!/bin/sh -eu
+#!/bin/sh -eux
 echo git pull
 git pull
 echo git add -A
 git add -A
-comment=`git status|grep -e deleted: -e renamed: -e modified: -e 'new file:'|tr '\n' ','|tr -s '     '|tr -d '#'|tr '\t' ' '`
-echo git commit -m "$comment comment_by_`basename $0`"
-git commit -m "$comment comment_by_`basename $0`"
-echo git push
-git push
+set +e
+git status | grep 'nothing to commit, working directory clean' > /dev/null
+if [ $? -ne 0 ];then
+	comment=`git status|grep -e deleted: -e renamed: -e modified: -e 'new file:'|tr '\n' ','|tr -s '     '|tr -d '#'|tr '\t' ' '`
+	echo git commit -m "$comment comment_by_`basename $0`"
+	git commit -m "$comment comment_by_`basename $0`"
+	echo git push
+	git push
+fi
+set -e
