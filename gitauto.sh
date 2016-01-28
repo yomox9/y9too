510262e3ecf1cf6332c09d038b9d8eb20c6aa143
yomox9 <yomox9@gmail.com>
2016-01-29 03:05:43 +0900

    modified: gitauto.sh, comment_by_gitpush.sh

--- gitauto.sh
+++ gitauto.sh
@@ -2,5 +2,5 @@
 
 #auto git
 set +e
-~nmrihserver/y9tool/b pushd ~/dotfiles ; sleep 1 && ~nmrihserver/y9tool/b gitpush.sh; sleep 1
-~nmrihserver/y9tool/b pushd ~/y9tool   ; sleep 1 && ~nmrihserver/y9tool/b gitpush.sh; sleep 1
+pushd ~/dotfiles ; sleep 1 && ~nmrihserver/y9tool/b gitpush.sh; sleep 1
+pushd ~/y9tool   ; sleep 1 && ~nmrihserver/y9tool/b gitpush.sh; sleep 1
