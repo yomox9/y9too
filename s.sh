1934010172485103c3c30f3ac01bdb250b6ba57c
yomox9 <yomox9@gmail.com>
2016-04-25 15:17:48 +0900

    modified: s.sh, comment_by_gitpush.sh

--- s.sh
+++ s.sh
@@ -4,10 +4,10 @@ cd /home/nmrihserver/serverfiles
 while :;do
 	echo ======================================================>>nmrih.log
 	grep -f p.txt nmrih/console_srv11.log |tail -16>>nmrih.log
-	echo `date "+%Y/%m/%d %H:%M:%S"` $0 ./srcds_run -game nmrih +map nmo_broadway -maxplayers 8 -insecure +servercfgfile server_isrv01.cfg +rcon_password $srcds_rcon_pass>>nmrih.log
+	echo `date "+%Y/%m/%d %H:%M:%S"` $0 ./srcds_run -game nmrih +map nmo_broadway -maxplayers 8 -insecure +servercfgfile server_srvXX.cfg +rcon_password $srcds_rcon_pass>>nmrih.log
 	df>>nmrih.log
-	echo ./srcds_run -game nmrih +map nmo_broadway -maxplayers 8 -insecure +servercfgfile server_isrv01.cfg +rcon_password $srcds_rcon_pass
-	./srcds_run -game nmrih +map nmo_broadway -maxplayers 6 -insecure +servercfgfile server_srv11.cfg +rcon_password $srcds_rcon_pass
+	echo ./srcds_run -game nmrih +map nmo_broadway -maxplayers 8 -insecure +servercfgfile server_srvXX.cfg +rcon_password $srcds_rcon_pass
+	./srcds_run -game nmrih +map nmo_broadway -maxplayers 6 -insecure +servercfgfile server_srvXX.cfg +rcon_password $srcds_rcon_pass
 
 done
 
